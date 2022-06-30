f043980362ee77aa690b34c69d4f6fd9011dfcac
ritik <RITIKDHEMBLA281@GMAIL.COM>
2022-06-30 18:22:19 +0530

    register

---
+++ register.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>LOGIN</title>
+      <style>
+      body {text-align:center;}
+    </style> 
+</head>
+<body>
+ <h1> REGISTER HERE</h1>
+<form action="registerprocess.php" method="post">
+
+<div class="form-group"><br></br>
+Name*=<input type="text" placeholder="ENTER YOUR NAME" name="Name">     
+</div>
+
+<div class="form-group"><br></br>
+Phone*=<input type="text" placeholder="ENTER YOUR PHONE_NO" name="Phone_no">     
+</div>
+
+<div class="form-group"><br></br>
+Password*=<input type="password" maxlength="6"placeholder="PASSWORD"name="Password" >
+</div>
+
+
+<br></br>
+
+<div class="form-group">
+Refer_id (optional)=<input type="text" placeholder="reffer id" name="sponsorid">
+</div>
+
+
+<br></br>
+
+<input type="SUBMIT"name="SUBMIT"value="SUBMIT">
+</form>
+
+
+
+
+</form>
+</body>
+</html>
